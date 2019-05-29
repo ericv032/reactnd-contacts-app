ae577d730ce62ad1045d972e0e032ae04e58a216
Eric Vazquez <ericvazquez@Erics-MBP.attlocal.net>
2019-05-28 21:16:05 -0400

    Added BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,39 +1,27 @@
 import React, { Component } from 'react'
-import ListContacts from './ListContacts';
+import ListContacts from './ListContacts'
 import PropTypes from 'prop-types'
+import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from './CreateContact';
+
 
 
 class App extends Component {
 
     state = {
 
-     contacts: [
-        {
-          "id": "karen",
-          "name": "Karen Isgrigg",
-          "handle": "karen_isgrigg",
-          "avatarURL": "http://localhost:5001/karen.jpg"
-        },
-        {
-          "id": "richard",
-          "name": "Richard Kalehoff",
-          "handle": "richardkalehoff",
-          "avatarURL": "http://localhost:5001/richard.jpg"
-        },
-        {
-          "id": "tyler",
-          "name": "Tyler McGinnis",
-          "handle": "tylermcginnis",
-          "avatarURL": "http://localhost:5001/tyler.jpg"
-        },
-        {
-          "id": "eric",
-          "name": "Eric Vazquez",
-          "handle": "ericvazquez",
-          "avatarURL": "http://localhost:5001/richard.jpg"
-        }
-       ]
+     contacts: [],
+     screen: 'list'
 
+    }
+
+    componentDidMount() {
+      ContactsAPI.getAll()
+      .then((contacts) => {
+        this.setState(() => ({
+          contacts
+        })) 
+      })
     }
 
     removeContact = (contact) => {
@@ -52,13 +40,14 @@ class App extends Component {
   render() {
     return (
       <div>
-      <ListContacts
+         
+      {this.state.screen === 'list' && <ListContacts
       contacts={this.state.contacts} 
       onDeleteContact={this.removeContact}
-      
-      />
+      />}
+     {this.state.screen === 'create' && <CreateContact />}
       </div>
-     
+    
     )
   }
 }
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
+import { Link } from 'react-router-dom'
 
 class ListContacts extends Component {
 
@@ -44,6 +44,10 @@ clearQuery = () => {
           value={query}
           onChange={(event) => this.updateQuery(event.target.value)}
           />
+              <Link
+          to="/create"
+          className="add-contact"
+          >Add Contact</Link>
         </form>
       </div>
       {showingContacts.length !== contacts.length && (
